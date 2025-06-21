ddfa0e1e0cb7ed1435d95a18e96e3f28fc0acc5a
Denis Kornev <denis@boostlingo.com>
2025-06-20 18:04:44 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Boostlingo",
-            url: "https://github.com/boostlingo/boostlingo-ios/releases/download/v2.0.0/Boostlingo.xcframework.zip",
+            url: "https://github.com/boostlingo/boostlingo-ios/releases/download/2.0.0/Boostlingo.xcframework.zip",
             checksum: "840df792419c29a2c0007c828691422df432425545fa80f77604b56013e78d93"
         ),
     ]
