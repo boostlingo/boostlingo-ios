3a735384bac16c27e2686efea1d70d9c27d65301
Denis Kornev <denis@boostlingo.com>
2025-06-23 14:59:10 -0700

    Update for 2.0.0.

--- Package.swift
+++ Package.swift
@@ -13,14 +13,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/moozzyk/SignalR-Client-Swift", from: "1.1.0"),
-        .package(url: "https://github.com/twilio/twilio-voice-ios", from: "6.13.1"),
+        .package(url: "https://github.com/twilio/twilio-voice-ios", from: "6.13.2"),
         .package(url: "https://github.com/twilio/twilio-video-ios", from: "5.9.0")
     ],
     targets: [
         .binaryTarget(
             name: "Boostlingo",
             url: "https://github.com/boostlingo/boostlingo-ios/releases/download/2.0.0/Boostlingo.xcframework.zip",
-            checksum: "8e92569fd32329e1edc78c5c96dcecf9eff96bd817f7da37e74968a5559b048c"
+            checksum: "838764a66c8617bb930d4321af798f8e2d0c86795d4b4119801f32f940b5f8a3"
         ),
     ]
 )
